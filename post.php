35a60d04e903a67fe9fb210a42e2b66777ef3894
Malvis Light <81842951+malvislight@users.noreply.github.com>
2022-04-07 02:54:51 +0300

    Create post.php

---
+++ post.php
@@ -0,0 +1,35 @@
+<?php
+
+$url = "https://reqbin.com/echo/post/json";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Authorization: Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = <<<DATA
+{
+  "Id": 12345,
+  "Customer": "John Smith",
+  "Quantity": 1
+}
+DATA;
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
+?>
+
